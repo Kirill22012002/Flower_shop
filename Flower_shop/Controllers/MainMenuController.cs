aa26b10b4398cc2b6b4fd5c70707814833b7bfe1
Кирилл Перепечкин <perepechkin.kirill@gmail.com>
2022-07-31 14:09:47 +0300

    Добавьте файлы проекта.

---
+++ Flower_shop/Controllers/MainMenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Flower_shop.Controllers
+{
+    public class MainMenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
