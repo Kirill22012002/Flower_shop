db9451e9c0e12c679ff960e965d1d0af312c0eda
Кирилл Перепечкин <perepechkin.kirill@gmail.com>
2022-08-04 18:25:00 +0300

    Добавил api yandex.map

---
+++ Flower_shop/wwwroot/js/placemark.js
@@ -0,0 +1,17 @@
+﻿ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [54.876674, 28.689605],
+        zoom: 17
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([54.876674, 28.689605], {
+            balloonContent: 'Цветы и подарки'
+        }, {
+            preset: 'islands#redSportIcon'
+        }));
+}
---
+++ Flower_shop/wwwroot/js/yandexMap.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(init());
+function init() {
+    ymaps.ready(function () {
+        var map = new ymaps.Map("map", {
+            center: [54.876674, 28.689605],
+            zoom: 50
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+    });
+};
