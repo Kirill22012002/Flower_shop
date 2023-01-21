4f66476417d00035c553ab629d05efa3f1b97e9f
Kirill22022002 <perepechkin_2002@mail.ru>
2023-01-21 15:08:12 +0300

    fix

--- Flower_shop/Controllers/PaymentController.cs
+++ Flower_shop/Controllers/PaymentController.cs
@@ -1,6 +1,7 @@
 ﻿using Flower_shop.EfStuff.DbModels;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Newtonsoft.Json;
+using System.Net;
 using Yandex.Checkout.V3;
 
 namespace Flower_shop.Controllers
@@ -46,7 +47,7 @@ namespace Flower_shop.Controllers
             Response.Redirect(url);
         }
 
-        [HttpPost]
+        /*[HttpPost]
         public void Paid(string code)
         {
             
@@ -57,6 +58,40 @@ namespace Flower_shop.Controllers
 
             _dbContext.Answers.Add(myAsnwer);
             _dbContext.SaveChanges();
+        }
+        public static async Task<string> GetPaymentDataFromYKassa(string url, string apiKey)
+        {
+            using (var client = new HttpClient())
+            {
+                // Set the headers
+                client.DefaultRequestHeaders.Add("X-API-KEY", apiKey);
+                // Get the response
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+                // Return the response
+                return content;
+            }
+        }*/
+        public string GetDataByHttpJson(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string data = reader.ReadToEnd();
+
+            var myAsnwer = new Answer()
+            {
+                notification_type = data
+            };
+
+            _dbContext.Answers.Add(myAsnwer);
+            _dbContext.SaveChanges();
+
+            return data;
+
+
         }
     }
 }
