1a236a971ef8fefc12bb2fab62eda02f2693baa8
Kirill22022002 <perepechkin_2002@mail.ru>
2023-01-21 14:42:10 +0300

    ifx

--- Flower_shop/Program.cs
+++ Flower_shop/Program.cs
@@ -30,14 +30,14 @@ builder.Services.AddSingleton(mapper);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+/*// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+}*/
 
 app.UseHttpsRedirection();
 
