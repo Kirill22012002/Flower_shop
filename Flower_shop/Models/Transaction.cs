b1cb487ef3e0c4d69f2436e9d6002151647f999c
Kirill22022002 <perepechkin_2002@mail.ru>
2023-01-22 09:16:44 +0300

    fix

--- Flower_shop/Models/Transaction.cs
+++ Flower_shop/Models/Transaction.cs
@@ -5,7 +5,6 @@ namespace Flower_shop.Models
 {
     public class Transaction
     {
-        public int Id { get; set; }
         public PaymentEvent Event { get; set; }
         public Payment Object { get; set; }
     }
